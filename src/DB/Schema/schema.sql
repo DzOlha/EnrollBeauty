2452cb2eea0c3bd0342708ec39a2e12e685b86f8
DzOlha <monsarda11@gmail.com>
2023-10-14 23:49:29 +0300

    Defined the database schema and created it in the phpMyAdmin tool

---
+++ src/DB/Schema/schema.sql
@@ -0,0 +1,412 @@
+CREATE DATABASE enrollbeauty
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE roles (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   name VARCHAR(50) UNIQUE NOT NULL,
+   created_date DATETIME NOT NULL
+);
+CREATE TABLE users_social (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      Instagram VARCHAR(100),
+      TikTok VARCHAR(100),
+      Facebook VARCHAR(100),
+      YouTube VARCHAR(100),
+      Google VARCHAR(100)
+);
+
+CREATE TABLE users_setting (
+       id INT AUTO_INCREMENT PRIMARY KEY,
+       recovery_code VARCHAR(100),
+       date_of_sending DATETIME
+);
+
+CREATE TABLE users_photo (
+     id INT AUTO_INCREMENT PRIMARY KEY,
+     name VARCHAR(255)
+);
+
+CREATE TABLE users (
+       id INT AUTO_INCREMENT PRIMARY KEY,
+       name VARCHAR(50) NOT NULL,
+       surname VARCHAR(50) NOT NULL,
+       password VARCHAR(255) NOT NULL,
+       email VARCHAR(100) UNIQUE NOT NULL,
+       social_id INT NOT NULL,
+       setting_id INT NOT NULL,
+       photo_id INT NOT NULL,
+       created_date DATETIME NOT NULL,
+       referral_parent_id INT,
+       last_login_date DATETIME,
+       role_id INT NOT NULL,
+       FOREIGN KEY (social_id) REFERENCES users_social(id),
+       FOREIGN KEY (setting_id) REFERENCES users_setting(id),
+       FOREIGN KEY (photo_id) REFERENCES users_photo(id),
+       FOREIGN KEY (role_id) REFERENCES roles(id)
+);
+
+
+CREATE TABLE workers_photo (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   name VARCHAR(255)
+);
+
+CREATE TABLE workers_social (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    Instagram VARCHAR(100),
+    LinkedIn VARCHAR(100),
+    Facebook VARCHAR(100),
+    Github VARCHAR(100),
+    Telegram VARCHAR(100),
+    YouTube VARCHAR(100),
+    TikTok VARCHAR(100)
+);
+CREATE TABLE workers_setting (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   recovery_code VARCHAR(100),
+   date_of_sending DATETIME
+);
+
+CREATE TABLE departments (
+     id INT AUTO_INCREMENT PRIMARY KEY,
+     name VARCHAR(50) NOT NULL
+);
+CREATE TABLE positions (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   name VARCHAR(50) NOT NULL,
+   department_id INT NOT NULL,
+   FOREIGN KEY (department_id) REFERENCES departments(id)
+);
+
+CREATE TABLE workers (
+     id INT AUTO_INCREMENT PRIMARY KEY,
+     name VARCHAR(50) NOT NULL,
+     surname VARCHAR(50) NOT NULL,
+     password VARCHAR(255) NOT NULL,
+     gender ENUM('Male', 'Female', 'Other'),
+     age INT NOT NULL,
+     years_of_experience DECIMAL(2, 2) NOT NULL,
+     position_id INT NOT NULL,
+     salary DECIMAL(10, 2),
+     hours_working INT,
+     photo_id INT,
+     social_id INT NOT NULL,
+     setting_id INT NOT NULL,
+     role_id INT NOT NULL,
+     FOREIGN KEY (position_id) REFERENCES positions(id),
+     FOREIGN KEY (photo_id) REFERENCES workers_photo(id),
+     FOREIGN KEY (social_id) REFERENCES workers_social(id),
+     FOREIGN KEY (setting_id) REFERENCES workers_setting(id),
+     FOREIGN KEY (role_id) REFERENCES roles(id)
+);
+CREATE TABLE documents (
+       id INT AUTO_INCREMENT PRIMARY KEY,
+       photo_name VARCHAR(255),
+       file_pdf_name VARCHAR(255)
+);
+CREATE TABLE workers_document (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      number VARCHAR(255) NOT NULL,
+      worker_id INT NOT NULL,
+      issued_by VARCHAR(255) NOT NULL,
+      issued_date DATE NOT NULL,
+      created_date DATETIME NOT NULL,
+      document_id INT NOT NULL,
+      FOREIGN KEY (worker_id) REFERENCES workers(id) ON DELETE CASCADE,
+      FOREIGN KEY (document_id) REFERENCES documents(id)
+);
+
+CREATE TABLE workers_rating (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    worker_id INT NOT NULL,
+    user_id INT NOT NULL,
+    score INT CHECK (score >= 0 AND score <= 5) NOT NULL,
+    created_date DATETIME NOT NULL,
+    FOREIGN KEY (worker_id) REFERENCES workers(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE affiliates (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    country VARCHAR(50) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    worker_manager_id INT NOT NULL,
+    created_date DATETIME NOT NULL,
+    FOREIGN KEY (worker_manager_id) REFERENCES workers(id)
+);
+
+CREATE TABLE workers_department_manager (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    affiliate_id INT NOT NULL,
+    department_id INT NOT NULL,
+    worker_id INT NOT NULL,
+    created_date DATETIME NOT NULL,
+    FOREIGN KEY (affiliate_id) REFERENCES affiliates(id) ON DELETE CASCADE,
+    FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE,
+    FOREIGN KEY (worker_id) REFERENCES workers(id) ON DELETE CASCADE
+);
+CREATE TABLE services (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      name VARCHAR(100) NOT NULL,
+      department_id INT NOT NULL,
+      FOREIGN KEY (department_id) REFERENCES departments(id)
+);
+CREATE TABLE orders_service (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,
+    service_id INT NOT NULL,
+    worker_id INT NOT NULL,
+    affiliate_id INT NOT NULL,
+    chosen_datetime DATETIME NOT NULL,
+    completed_datetime DATETIME,
+    canceled_datetime DATETIME,
+    price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (service_id) REFERENCES services(id) ON DELETE CASCADE,
+    FOREIGN KEY (worker_id) REFERENCES workers(id) ON DELETE CASCADE,
+    FOREIGN KEY (affiliate_id) REFERENCES affiliates(id) ON DELETE CASCADE
+);
+CREATE TABLE categories (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    parent_category_id INT,
+    FOREIGN KEY (parent_category_id) REFERENCES categories(id) ON DELETE CASCADE
+);
+CREATE TABLE products (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      name VARCHAR(100) NOT NULL,
+      price DECIMAL(10, 2) NOT NULL,
+      category_id INT NOT NULL,
+      available_count INT NOT NULL,
+      FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
+);
+CREATE TABLE orders_product (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,
+    product_id INT NOT NULL,
+    count INT NOT NULL,
+    worker_id INT,
+    affiliate_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE,
+    FOREIGN KEY (worker_id) REFERENCES workers(id) ON DELETE CASCADE,
+    FOREIGN KEY (affiliate_id) REFERENCES affiliates(id)
+);
+
+CREATE TABLE comments (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      user_id INT NOT NULL,
+      text TEXT NOT NULL,
+      created_date DATETIME NOT NULL,
+      FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+
+# Statistics
+CREATE TABLE periods (
+     id INT AUTO_INCREMENT PRIMARY KEY,
+     name VARCHAR(30) NOT NULL,
+     start_date DATE NOT NULL,
+     end_date DATE NOT NULL,
+     created_date DATETIME NOT NULL,
+     updated_date DATETIME
+);
+
+#   Statistics: Spends
+CREATE TABLE spend_types (
+     id INT AUTO_INCREMENT PRIMARY KEY,
+     name VARCHAR(100) UNIQUE NOT NULL,
+     created_date DATETIME NOT NULL
+);
+CREATE TABLE statistic_spends (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      spend_type_id INT NOT NULL,
+      sum DECIMAL(10, 2) NOT NULL,
+      created_date DATETIME NOT NULL,
+      FOREIGN KEY (spend_type_id) REFERENCES spend_types(id) ON DELETE CASCADE
+);
+CREATE TABLE statistic_spends_summary (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      spend_type_id INT NOT NULL,
+      sum DECIMAL(10, 2) NOT NULL,
+      period_id INT NOT NULL,
+      created_date DATETIME NOT NULL,
+      updated_date DATETIME,
+      FOREIGN KEY (spend_type_id) REFERENCES spend_types(id) ON DELETE CASCADE,
+      FOREIGN KEY (period_id) REFERENCES periods(id) ON DELETE CASCADE
+);
+
+# Statistics: Incomes
+CREATE TABLE income_types (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      name VARCHAR(100) UNIQUE NOT NULL,
+      created_date DATETIME NOT NULL
+);
+CREATE TABLE statistic_incomes (
+       id INT AUTO_INCREMENT PRIMARY KEY,
+       income_type_id INT NOT NULL,
+       sum DECIMAL(10, 2) NOT NULL,
+       created_date DATETIME NOT NULL,
+       FOREIGN KEY (income_type_id) REFERENCES income_types(id) ON DELETE CASCADE
+);
+CREATE TABLE statistic_incomes_summary (
+       id INT AUTO_INCREMENT PRIMARY KEY,
+       income_type_id INT NOT NULL,
+       sum DECIMAL(10, 2) NOT NULL,
+       period_id INT NOT NULL,
+       created_date DATETIME NOT NULL,
+       updated_date DATETIME,
+       FOREIGN KEY (income_type_id) REFERENCES income_types(id) ON DELETE CASCADE,
+       FOREIGN KEY (period_id) REFERENCES periods(id) ON DELETE CASCADE
+);
+
+# Statistic: Workers [Income for the salon from worker]
+CREATE TABLE statistic_workers_income (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      worker_id INT NOT NULL,
+      type_id INT NOT NULL,
+      created_date DATETIME NOT NULL,
+      sum DECIMAL(10, 2) NOT NULL,
+      FOREIGN KEY (worker_id) REFERENCES workers(id) ON DELETE CASCADE,
+      FOREIGN KEY (type_id) REFERENCES income_types(id) ON DELETE CASCADE
+);
+CREATE TABLE statistic_workers_income_summary (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      worker_id INT NOT NULL,
+      sum DECIMAL(10, 2) NOT NULL,
+      period_id INT NOT NULL,
+      created_date DATETIME NOT NULL,
+      updated_date DATETIME,
+      FOREIGN KEY (worker_id) REFERENCES workers(id) ON DELETE CASCADE,
+      FOREIGN KEY (period_id) REFERENCES periods(id) ON DELETE CASCADE
+);
+# Statistic: Workers [Earning of the worker]
+CREATE TABLE workers_earning_types (
+       id INT AUTO_INCREMENT PRIMARY KEY,
+       name VARCHAR(50) NOT NULL
+);
+CREATE TABLE statistic_workers_earning (
+       id INT AUTO_INCREMENT PRIMARY KEY,
+       worker_id INT NOT NULL,
+       sum DECIMAL(10, 2) NOT NULL,
+       type_id INT NOT NULL,
+       created_date DATETIME NOT NULL,
+       FOREIGN KEY (worker_id) REFERENCES workers(id) ON DELETE CASCADE,
+       FOREIGN KEY (type_id) REFERENCES workers_earning_types(id) ON DELETE CASCADE
+);
+CREATE TABLE statistic_workers_earning_summary (
+       id INT AUTO_INCREMENT PRIMARY KEY,
+       worker_id INT NOT NULL,
+       sum DECIMAL(10, 2) NOT NULL,
+       period_id INT NOT NULL,
+       created_date DATETIME NOT NULL,
+       updated_date DATETIME,
+       FOREIGN KEY (worker_id) REFERENCES workers(id) ON DELETE CASCADE,
+       FOREIGN KEY (period_id) REFERENCES periods(id) ON DELETE CASCADE
+);
+
+# Statistic: Users
+CREATE TABLE statistic_users_service (
+     id INT AUTO_INCREMENT PRIMARY KEY,
+     user_id INT NOT NULL,
+     order_id INT NOT NULL,
+     created_date DATETIME NOT NULL,
+     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+     FOREIGN KEY (order_id) REFERENCES orders_service(id) ON DELETE CASCADE
+);
+CREATE TABLE statistic_users_product (
+     id INT AUTO_INCREMENT PRIMARY KEY,
+     user_id INT NOT NULL,
+     order_id INT NOT NULL,
+     created_date DATETIME NOT NULL,
+     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+     FOREIGN KEY (order_id) REFERENCES orders_product(id) ON DELETE CASCADE
+);
+CREATE TABLE statistic_users_service_summary (
+     id INT AUTO_INCREMENT PRIMARY KEY,
+     user_id INT NOT NULL,
+     sum DECIMAL(10, 2) NOT NULL,
+     period_id INT NOT NULL,
+     created_date DATETIME NOT NULL,
+     updated_date DATETIME,
+     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+     FOREIGN KEY (period_id) REFERENCES periods(id) ON DELETE CASCADE
+);
+CREATE TABLE statistic_users_product_summary (
+     id INT AUTO_INCREMENT PRIMARY KEY,
+     user_id INT NOT NULL,
+     sum DECIMAL(10, 2) NOT NULL,
+     period_id INT NOT NULL,
+     created_date DATETIME NOT NULL,
+     updated_date DATETIME,
+     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+     FOREIGN KEY (period_id) REFERENCES periods(id) ON DELETE CASCADE
+);
+
+# Statistic: Services
+CREATE TABLE statistic_services (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    service_id INT NOT NULL,
+    order_id INT NOT NULL,
+    created_date DATETIME NOT NULL,
+    FOREIGN KEY (service_id) REFERENCES services(id) ON DELETE CASCADE,
+    FOREIGN KEY (order_id) REFERENCES orders_service(id) ON DELETE CASCADE
+);
+CREATE TABLE statistic_services_summary (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    service_id INT NOT NULL,
+    sum DECIMAL(10, 2) NOT NULL,
+    period_id INT NOT NULL,
+    created_date DATETIME NOT NULL,
+    updated_date DATETIME,
+    FOREIGN KEY (service_id) REFERENCES services(id) ON DELETE CASCADE,
+    FOREIGN KEY (period_id) REFERENCES periods(id) ON DELETE CASCADE
+);
+
+# Statistic: Products
+CREATE TABLE statistic_products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INT NOT NULL,
+    order_id INT NOT NULL,
+    created_date DATETIME NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE,
+    FOREIGN KEY (order_id) REFERENCES orders_product(id) ON DELETE CASCADE
+);
+CREATE TABLE statistic_products_summary (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INT NOT NULL,
+    sum DECIMAL(10, 2) NOT NULL,
+    period_id INT NOT NULL,
+    created_date DATETIME NOT NULL,
+    updated_date DATETIME,
+    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE,
+    FOREIGN KEY (period_id) REFERENCES periods(id) ON DELETE CASCADE
+);
+
+# Statistic: Categories
+CREATE TABLE statistic_categories (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      category_id INT NOT NULL,
+      order_id INT NOT NULL,
+      created_date DATETIME NOT NULL,
+      FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE,
+      FOREIGN KEY (order_id) REFERENCES orders_product(id) ON DELETE CASCADE
+);
+CREATE TABLE statistic_categories_summary (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      category_id INT NOT NULL,
+      sum DECIMAL(10, 2) NOT NULL,
+      period_id INT NOT NULL,
+      created_date DATETIME NOT NULL,
+      updated_date DATETIME NOT NULL,
+      FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE,
+      FOREIGN KEY (period_id) REFERENCES periods(id) ON DELETE CASCADE
+);
